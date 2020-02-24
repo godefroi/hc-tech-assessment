7c2a43f28e4402bbbd4109467419a58a86af519d
Mark Parker <mparker@medicity.com>
2020-02-24 11:07:36 -0700

    Ensure System.Web is loaded on PS desktop

--- PeopleSearchModule/PeopleSearch.psm1
+++ PeopleSearchModule/PeopleSearch.psm1
@@ -1,2 +1,6 @@
+if( $PSVersionTable.PSEdition -eq 'Desktop' ) {
+	add-type -AssemblyName 'System.Web'
+}
+
 # just dot-source all the .ps1 files
 get-childitem $psscriptroot -filter '*.ps1' | foreach { . $_.fullname }
