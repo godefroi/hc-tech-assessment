ba531a0a1a71c4c3a83e443f632b1a8a80c0386f
Mark Parker <mparker@medicity.com>
2020-02-24 11:10:48 -0700

    Disable parallel tests... dirty, but works

---
+++ PeopleSearchTests/Configuration.cs
@@ -0,0 +1,7 @@
+﻿using Xunit;
+
+// We disable test parallelization because we've hard-coded the database
+// location, and running all the tests in parallel means everything is
+// racing, and causes intermittent failures. A better soloution would be
+// to have each test create its own database, but this is a sample...
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
